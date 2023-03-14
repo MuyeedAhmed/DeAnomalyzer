64942123b99ddcf4eac53c8b14d25496cb58d991
Muyeed Ahmed <muyeedahmed@Muyeeds-MacBook-Air.local>
2023-03-13 20:31:28 -0400

    Update 2

--- DeAnomalyzer.py
+++ DeAnomalyzer.py
@@ -12,6 +12,7 @@ import pandas as pd
 import mat73
 from scipy.io import loadmat
 from sklearn.ensemble import IsolationForest
+from sklearn.neighbors import LocalOutlierFactor
 import numpy as np
 from sklearn import metrics
 from copy import copy, deepcopy
@@ -32,8 +33,37 @@ class DeAnomalyzer:
         self.X = []
         self.y = []
         
+        self.readData()
+        self.injectParameter()
         
-        
+    def injectParameter(self):
+        if self.algoName == "IF" and self.tool == "Sklearn":
+            auto_1 = min(256, self.X.shape[0])/self.X.shape[0]
+            bisect.insort(self.parameters[1][2], auto_1)
+            self.parameters[1][2][self.parameters[1][2].index(auto_1)] = 'auto'
+        elif self.algoName == "IF" and self.tool == "Matlab":
+            if_cont, lof_cont = self.IF_LOF_ContFactor(self.X)
+            if lof_cont != 0:
+                bisect.insort(self.parameters[0][2], lof_cont)
+            if if_cont != 0:
+                bisect.insort(self.parameters[0][2], if_cont)
+            
+            auto_1 = min(256, self.X.shape[0])/self.X.shape[0]
+            bisect.insort(self.parameters[2][2], auto_1)
+            self.parameters[2][2][self.parameters[2][2].index(auto_1)] = 'auto'
+        elif self.algoName == "EE" and self.tool == "Sklearn":
+            if_cont, lof_cont = self.IF_LOF_ContFactor(self.X)
+            if lof_cont != 0:
+                bisect.insort(self.parameters[3][2], lof_cont)
+            if if_cont != 0:
+                bisect.insort(self.parameters[3][2], if_cont)
+        elif self.algoName == "OCSVM" and self.tool == "Matlab":
+            if_cont, lof_cont = self.IF_LOF_ContFactor(self.X)
+            if lof_cont != 0:
+                bisect.insort(self.parameters[0][2], lof_cont)
+            if if_cont != 0:
+                bisect.insort(self.parameters[0][2], if_cont)
+      
     def readData(self):
         folderpath = datasetFolderDir
         if os.path.exists(folderpath+self.fileName+".mat") == 1:
@@ -66,6 +96,30 @@ class DeAnomalyzer:
             print("Error: File doesn't exist")
             return
     
+    def IF_LOF_ContFactor(self):
+        labels = []
+        num_label = 0
+        for i in range(5):
+            clustering = IsolationForest().fit(self.X)
+        
+            l = clustering.predict(self.X)
+            num_label = len(l)
+            l = [0 if x == 1 else 1 for x in l]
+            labels.append(l)
+        _, counts_if = np.unique(labels, return_counts=True)
+        if_per = min(counts_if)/(num_label*5)
+    
+        
+        labels_lof = LocalOutlierFactor().fit_predict(self.X)
+        _, counts_lof = np.unique(labels_lof, return_counts=True)
+        lof_per = min(counts_lof)/(num_label)
+        
+        if if_per == 1:
+            if_per = 0
+        if lof_per == 1:
+            lof_per = 0
+        
+        return if_per, lof_per 
     
     def get_blind_route(self):
         blind_route = []
@@ -176,14 +230,110 @@ class DeAnomalyzer:
             guided_route.append([self.parameters[p][0], max_index, default_index, parameter_route])
         return guided_route
 
-    def runAlgo(self):
-        print("Incomplete")
+    def runAlgo(self, params):
         
+        import matlab.engine
+        eng = matlab.engine.start_matlab()
+        print("fix eng")
+        labelFile = self.fileName + "_" + str(params[0][1]) + "_" + str(params[1][1]) + "_" + str(params[2][1]) + "_" + str(params[3][1]) + "_" + str(params[4][1]) + "_" + str(params[5][1]) + "_" + str(params[6][1]) + "_" + str(params[7][1])
+        
+        if os.path.exists("Labels/OCSVM_Matlab/Labels_Mat_OCSVM_"+labelFile+".csv") == 0:
+            frr=open("GD_ReRun/MatOCSVM.csv", "a")
+            frr.write(self.fileName+","+str(params[0][1])+","+str(params[1][1])+","+str(params[2][1])+","+str(params[3][1])+","+str(params[4][1])+","+str(params[5][1])+","+str(params[6][1])+","+str(params[7][1])+'\n')
+            frr.close()
+            try:
+                eng.MatOCSVM_Rerun(nargout=0)
+                frr=open("GD_ReRun/MatOCSVM.csv", "w")
+                frr.write('Filename,ContaminationFraction,KernelScale,Lambda,NumExpansionDimensions,StandardizeData,BetaTolerance,BetaTolerance,GradientTolerance,IterationLimit\n')
+                frr.close()
+                if os.path.exists("Labels/OCSVM_Matlab/Labels_Mat_OCSVM_"+labelFile+".csv") == 0:      
+                    print("\nFaild to run Matlab Engine from Python.\n")
+                    exit(0)
+            except:
+                print("\nFaild to run Matlab Engine from Python.\n")
+                exit(0)    
+        f1 = []
+        ari = []
+        
+        labels =  pd.read_csv("Labels/OCSVM_Matlab/Labels_Mat_OCSVM_"+labelFile+".csv", header=None).to_numpy()
+        
+
+
+
+        if self.withGT:
+            for i in range(10):
+                f1.append(metrics.f1_score(self.y, labels[i]))
+            
+        for i in range(len(labels)):
+            for j in range(i+1, len(labels)):
+              ari.append(adjusted_rand_score(labels[i], labels[j]))
+              
+        if self.withGT:
+            return np.mean(f1), np.mean(ari)
+        else:
+            return -1, np.mean(ari) 
+
+def runOCSVM(filename, X, gt, params, parameter_iteration):
+    labelFile = filename + "_" + str(params[0][1]) + "_" + str(params[1][1]) + "_" + str(params[2][1]) + "_" + str(params[3][1]) + "_" + str(params[4][1]) + "_" + str(params[5][1]) + "_" + str(params[6][1]) + "_" + str(params[7][1])
+
+    if os.path.exists("Labels/OCSVM_Matlab/Labels_Mat_OCSVM_"+labelFile+".csv") == 0:
+        frr=open("GD_ReRun/MatOCSVM.csv", "a")
+        frr.write(filename+","+str(params[0][1])+","+str(params[1][1])+","+str(params[2][1])+","+str(params[3][1])+","+str(params[4][1])+","+str(params[5][1])+","+str(params[6][1])+","+str(params[7][1])+'\n')
+        frr.close()
+        try:
+            eng.MatOCSVM_Rerun(nargout=0)
+            frr=open("GD_ReRun/MatOCSVM.csv", "w")
+            frr.write('Filename,ContaminationFraction,KernelScale,Lambda,NumExpansionDimensions,StandardizeData,BetaTolerance,BetaTolerance,GradientTolerance,IterationLimit\n')
+            frr.close()
+            if os.path.exists("Labels/OCSVM_Matlab/Labels_Mat_OCSVM_"+labelFile+".csv") == 0:      
+                print("\nFaild to run Matlab Engine from Python.\n")
+                exit(0)
+        except:
+            print("\nFaild to run Matlab Engine from Python.\n")
+            exit(0)    
+    f1 = []
+    ari = []
+    
+    labels =  pd.read_csv("Labels/OCSVM_Matlab/Labels_Mat_OCSVM_"+labelFile+".csv", header=None).to_numpy()
+    
         
 if __name__ == '__main__':
-    
     da = DeAnomalyzer("IF", "Sklearn", "ar1")
     
+    blind_route = da.get_blind_route()
+    
+    DefaultARI = str(blind_route[0][3][0][1])
+    DefaultF1 = str(blind_route[0][3][0][2])
+    print("Default settings: ")
+    print("\tCross-run ARI: ", DefaultARI)
+    if da.withGT:
+        print("\tF1 Score: ", DefaultF1)
+    
+    UninformedARI = str(blind_route[-1][3][-1][1])
+    UninformedF1 = str(blind_route[-1][3][-1][2])
+    print("Univariate Search: ")
+    print("\tCross-run ARI: ", UninformedARI)
+    if da.withGT:
+        print("\tF1 Score: ", UninformedF1)
+    print("\tOutput Parameters:")
+    print("\n\t", end='')
+    for i in range(len(blind_route)):
+        print(blind_route[i][0],":", blind_route[i][3][blind_route[i][1]][0], end=', ')
+    print("\n")
+    
+    if da.withGT:
+        guided_route = da.get_guided_route()
+    
+        InformedARI = str(guided_route[-1][3][-1][1])
+        InformedF1 = str(guided_route[-1][3][-1][2])
+        print("Bivariate Search: ")
+        print("\tCross-run ARI: ", InformedARI)
+        print("\tF1 Score: ", InformedF1)
+        print("\tOutput Parameters:")
+        print("\n\t", end='')
+        for i in range(len(guided_route)):
+            print(guided_route[i][0],":", guided_route[i][3][guided_route[i][1]][0], end=', ')
+        print("\n")
     # print("DeAnomalyzer\n\n")
     # print("Select one of the following action by entering the serial number")
     # print("\t1. Reduce Non-determinism in Isolation Forest: Scikit-Learn")
